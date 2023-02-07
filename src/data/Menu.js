55df4e7d065bced8951ffd91fd35af87e7a23e0d
shay-e-ls <shaysha6@gmail.com>
2023-02-07 17:54:56 +0200

    added menu json

---
+++ src/data/Menu.js
@@ -0,0 +1,95 @@
+export const menu = [
+  {
+    title: 'Home'
+  },
+  {
+    title: 'Menu 1',
+    children: [
+      {
+        title: 'Sub menu 1',
+      },
+      {
+        title: 'Sub menu 2',
+      },
+      {
+        title: 'Sub menu 3',
+      },
+      {
+        title: 'Sub menu 4',
+        children: [
+          {
+            title: 'Deep Menu 1',
+            children: [
+              {
+                title: 'Sub Deep Menu 1',
+              },
+              {
+                title: 'Sub Deep Menu 2',
+              },
+            ]
+          },
+          {
+            title: 'Deep Menu 2',
+          },
+        ],
+      },
+      {
+        title: 'Sub menu 5',
+      },
+    ],
+  },
+  {
+    title: 'Menu 2',
+    children: [
+      {
+        title: 'Sub menu 1',
+      },
+    ],
+  },
+  {
+    title: 'Menu 3',
+    children: [
+      {
+        title: 'Sub menu 1',
+      },
+      {
+        title: 'Sub Menu 2 THIS IS SO LONG IT MIGHT CAUSE AN ISSEUE BUT MAYBE NOT?',
+        children: [
+          {
+            title: 'Category 1',
+          },
+          {
+            title: 'Category 2',
+          },
+          {
+            title: 'Category 3',
+          },
+          {
+            title: 'Category 4',
+          },
+          {
+            title: 'Category 5',
+          }
+        ],
+      },
+      {
+        title: 'Sub menu 3',
+      },
+      {
+        title: 'Sub menu 4',
+      },
+    ],
+  },
+  {
+    title: 'Menu 4'
+  },
+  {
+    title: 'Menu 5'
+  },
+  {
+    title: 'Menu 6'
+  },
+  {
+    title: 'Contact Us'
+  },
+];
